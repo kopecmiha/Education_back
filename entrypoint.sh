2ea6cd4c8c81b1e89a726d51bb9c26535dde5618
Михаил <koptseff.mikhail@yandex.ru>
2021-11-08 20:31:34 +0300

    some fixes

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-gunicorn Djoker.wsgi:application --bind 0.0.0.0:8000
